d7e5d5089e03bb5e7d06a758cb4a819182421d9a
Joel <jken223@aucklanduni.ac.nz>
2025-09-24 20:33:06 +1200

    both clarity in one figure

--- MATLAB/mainC.m
+++ MATLAB/mainC.m
@@ -3,17 +3,25 @@ clear;
 
 % Load IRs into a cell array
 fileNames = [ ...
-    "./../Recordings/Lecture theater/Lecture Theater Sep 20 0.5m-48k.wav", ...
-    "./../Recordings/Lecture theater/Lecture Theater Sep 20 1m-48k.wav", ...
-    "./../Recordings/Lecture theater/Lecture Theater Sep 20 2m-48k.wav", ...
-    "./../Recordings/Lecture theater/Lecture Theater Sep 20 3m-48k.wav"];
+    "./../Recordings/Lecture theater/Lecture Theater Sep 20 back 0.5m-48k.wav", ...
+    "./../Recordings/Lecture theater/Lecture Theater Sep 20 back 1m-48k.wav", ...
+    "./../Recordings/Lecture theater/Lecture Theater Sep 20 back 2m-48k.wav", ...
+    "./../Recordings/Lecture theater/Lecture Theater Sep 20 back 3m-48k.wav"];
 
-clarity = zeros(length(fileNames), 2);
+staffFilenames = [ ...
+    "./../Recordings/Staff room/staff room 0.5m loc 2-48k.wav", ...
+    "./../Recordings/Staff room/staff room 1m loc 2-48k.wav", ...
+    "./../Recordings/Staff room/staff room 2m loc 2-48k.wav", ...
+    "./../Recordings/Staff room/staff room 33m loc 2-48k.wav"];
+
+clarity1 = zeros(length(fileNames), 2);
+clarity2 = zeros(length(staffFilenames), 2);
 
 distances = [0.5, 1, 2, 3]; 
 
 for i = 1:length(fileNames)
     [ir, fs] = audioread(fileNames{i});
+    ir = trim_to_direct(ir);
 
     ir2 = ir.^2;
     
@@ -24,15 +32,39 @@ for i = 1:length(fileNames)
     
     C50 = 10*log10(earlyEnergy / lateEnergy);
     
-    clarity(i,:) = [distances(i), C50];
+    clarity1(i,:) = [distances(i), C50];
+end
+
+for i = 1:length(staffFilenames)
+    [ir, fs] = audioread(staffFilenames{i});
+    ir = trim_to_direct(ir);
+
+    ir2 = ir.^2;
+    
+    cutoffSample = round(0.05 * fs); 
+    
+    earlyEnergy = sum(ir2(1:cutoffSample));
+    lateEnergy  = sum(ir2(cutoffSample+1:end));
+    
+    C50 = 10*log10(earlyEnergy / lateEnergy);
+    
+    clarity2(i,:) = [distances(i), C50];
 end
 
 figure;
-plot(clarity(:,1), clarity(:,2), 'LineWidth', 1.3);
+subplot(1, 2, 1)
+plot(clarity1(:,1), clarity1(:,2), '-x', 'LineWidth', 1.3);
 grid on;
 xlabel('Distance between speaker and microphone (m)');
 ylabel('Clarity C_{50} (dB)');
 title('C_{50} with distance - Lecture Theatre');
+
+subplot(1, 2, 2)
+plot(clarity2(:,1), clarity2(:,2), '-x', 'LineWidth', 1.3);
+grid on;
+xlabel('Distance between speaker and microphone (m)');
+ylabel('Clarity C_{50} (dB)');
+title('C_{50} with distance - Staff Room');
 
 function x = trim_to_direct(x)
     % keep a handful of samples before the absolute-peak (direct sound)
